ea90a2f68fb6a2fc771d4a0c6392fb8f2225ebdf
vanajapulivarthi <54466610+vanajapulivarthi@users.noreply.github.com>
2022-10-14 19:26:20 +0530

    Update mainpy.py

--- mainpy.py
+++ mainpy.py
@@ -14,7 +14,7 @@ from streamlit_option_menu import option_menu
 
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_disease_model (2).sav','rb'))
 
 parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
